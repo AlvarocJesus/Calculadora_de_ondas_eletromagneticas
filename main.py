58569a665d6fbd88c10952a9d2e96824669d76d5
AlvarocJesus <alvim.coelho.jesus@gmail.com>
2022-08-18 08:40:37 -0300

    Adiciona estrutura inicial da calculadora

---
+++ main.py
@@ -0,0 +1,16 @@
+print("""
+Integrantes do grupo:
+Álvaro Coelho Jesus
+Fernando Shiraishi
+Vinícius Nanaki
+""")
+
+while(True):
+  print('teste')
+  option = int(input('Digite uma opção:'))
+  if():
+    print()
+  elif():
+    print()
+  elif():
+    print()
