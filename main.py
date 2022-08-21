a47c8136d3c3ce405077384b5af5389ad44b0497
AlvarocJesus <alvim.coelho.jesus@gmail.com>
2022-08-21 17:24:14 -0300

    Altera exibicao do menu principal

--- main.py
+++ main.py
@@ -7,25 +7,26 @@ Vinícius Nanaki
 
 
 def main():
-  while(True):
-    print("\n-----------------Menu-----------------");
-    print("Indique a sua entrada:")
-    print("1 - Amplitude Campo Elétrico")
-    print("2 - Amplitude Campo Magnético")
-    print("3 - Intensidade da onda")
-    print("4 - Comprimento de onda")
-    print("5 - Frequência")
-    print("6 - Frequência angular")
-    print("7 - Número de onda")
-    print("")
-    print("0 - Sair")
-    print("")
-    option = int(input('Digite uma opção:'))
-    if(option == 0):
-        return
-    elif(option == 1):
-      print()
-    elif():
-      print()
+	while(True):
+		print("\n-----------------Menu-----------------")
+		print("""Indique a sua entrada:
+1 - Amplitude Campo Elétrico
+2 - Amplitude Campo Magnético
+3 - Intensidade da onda
+4 - Comprimento de onda
+5 - Frequência
+6 - Frequência angular
+7 - Número de onda
 
-main()      
+0 - Sair
+		""")
+		option = float(input('Digite uma opção:'))
+		if(option == 0):
+			return
+		elif(option == 1):
+			print()
+		elif():
+			print()
+
+
+main()
